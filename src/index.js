3650a40e3440c3873db2fae1e699475b42b118d2
Artem Matyushevskiy <artemmatyushevskiy@gmail.com>
2019-07-13 20:22:52 +0300

    js

---
+++ src/index.js
@@ -0,0 +1,66 @@
+/**
+ * LightningChartJS example that shows the creation and styling of a Funnel chart.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const { 
+    FunnelChartDesigns, 
+    FunnelLabelSide, 
+    SliceLabelFormatters,
+    lightningChart,
+    SolidFill, 
+    FunnelSliceModes,  
+    ColorPalettes,
+    LegendBoxBuilders,
+    UIOrigins
+} = lcjs
+
+// Create a Funnel chart
+const funnel = lightningChart()
+    .Funnel( { design: FunnelChartDesigns.LabelsOnSides } )
+    .setSliceMode( FunnelSliceModes.VariableHeight )
+    .setTitle( 'Customer contacts progression' )
+    .setHeadWidth( 95 )
+    .setNeckWidth( 40 )
+    .setSliceGap( 0 )
+    .setLabelSide( FunnelLabelSide.Right )
+
+// Data for all slices
+const data = [
+    {
+        name: 'Prospects',
+        value: 2000
+        }, {
+        name: 'Contacts',
+        value: 1540
+        }, {
+        name: 'Leads',
+        value: 1095
+        }, {
+        name: 'Customers',
+        value: 549
+        }
+]
+// Add data to the slices
+const slices = data.forEach( ( slice ) => {
+    funnel.addSlice( slice.name, slice.value )
+} )
+
+// Set formatter of Slice Labels
+funnel.setLabelFormatter( SliceLabelFormatters.NamePlusValue )
+
+// Create FillStyle Palette for Funnel (defines color of Slice filling)
+const palette = ColorPalettes.warm( 4 )
+// FillStyle Palette is a function which returns FillStyle based on index.
+const fillStylePalette = ( i ) => new SolidFill().setColor( palette( i ) )
+funnel.setSliceFillStyle(fillStylePalette )
+
+// Add LegendBox and define the position in the chart
+const lb = funnel
+    .addLegendBox( LegendBoxBuilders.VerticalLegendBox )
+    .setPosition( { x: 0, y: 0 } )
+    .setOrigin( UIOrigins.LeftBottom )
+    .setMargin( 5 )
+lb.add( funnel, false )
